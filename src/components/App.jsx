992ee681d5bb23de13ac94037fab30ecf953fe93
CF <farsee37@gmail.com>
2025-07-22 23:34:03 -0500

    react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
 
-import{ useState } from 'react';
+import{ use, useState } from 'react';
+import { useNavigate, Link} from 'react-router-dom';
+
 import { Routes, Route } from "react-router-dom";
-import { Fish, Quote, Register, Navbar, Login } from './index';
+import { Fish, Quote, Register, Login } from './index';
+
+import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';  
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -15,14 +20,66 @@ export default function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 console.log('Token in App:', token);
+
+    async function logout() {
+        setToken("")
+        setIsLoggedIn(false)
+        window.localStorage.removeItem("token")
+        useNavigate(window.location.href="/login")
+    }
+
+  function Navbar({ isLoggedIn, setToken, setIsLoggedIn }) {
+  console.log('Navbar isLoggedIn:', isLoggedIn);
+  const handleMenuClick = () => {
+  console.log("Menu clicked! You can add a drawer here.");
+  };
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleMenuClick} // Handling menu click
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Test App
+          </Typography>
+         <Button color="inherit" onClick={logout}>Logout</Button>
+          <Link to="/login" style={{ textDecoration: 'none' }}>  {/* Link for SPA navigation */}
+            <Button color="inherit">Login</Button>
+          </Link>
+          <Link to="/register" style={{ textDecoration: 'none' }}>  {/* Link for SPA navigation */}
+            {isLoggedIn ? <Button color="inherit">Register</Button> : null}
+          </Link>
+          <Link to="/fish" style={{ textDecoration: 'none' }}>  {/* Link for SPA navigation */}
+            <Button color="inherit">Fish</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
   return (
     <div>
       <h1>Test-App</h1>
-      <Navbar />
+      <Navbar 
+        isLoggedIn={isLoggedIn} 
+        setToken={setToken} 
+        setIsLoggedIn={setIsLoggedIn} 
+      />
       <Quote />
       <Routes>
         <Route path='/login' 
-        element={<Login 
+          element={<Login 
           username={username}
           setUsername={setUsername}
           password={password}
@@ -36,11 +93,12 @@ console.log('Token in App:', token);
         />} />
 
         <Route path="/fish" 
-        element={<Fish />} />
+          element={<Fish />} />
 
         <Route path="/register" 
-        element={<Register />} />
+          element={<Register />} />
 
+     
       </Routes>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export { default as App } from './App';
 export { default as Quote } from './Quote';
 export { default as Fish } from './Fish';
 export { default as Register } from './Register';
-export { default as Navbar } from './Navbar';
+
 export { default as Login } from './Login';
